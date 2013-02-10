709e81f5f6adf11b6a154c20817ebef9d56a0dac
Yotam Mann <yotammann@gmail.com>
2013-02-10 15:50:14 -0500

    working on server-side structure

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var db = require('./node_modules/database');
+var crawler = require('./node_modules/crawler');
+var app = express();
+
+/* 
+ * BASIC SERVER
+ */
+
+// log requests
+app.use(express.logger('dev'));
+
+//data base query
+app.use('/db', db.getPost);
+
+//static file server
+app.use(express.static(__dirname + '/public'));
+
+//start the server
+app.listen(3000, "127.0.0.1");
+
+//periodic crawling
+setInterval(crawler.update, 60000);
+
+//print a message
+console.log('Listening on port 3000');
